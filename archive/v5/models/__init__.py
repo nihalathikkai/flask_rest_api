061de1e85b562b134c874c1c693eaaa545306f4c
nihalathikkai <nihalathikkai1999@gmail.com>
2024-04-08 16:49:24 +0530

    added archive v5

---
+++ archive/v5/models/__init__.py
@@ -0,0 +1,2 @@
+from models.store import StoreModel
+from models.items import ItemModel
---
+++ archive/v5/models/items.py
@@ -0,0 +1,28 @@
+from db import db
+
+# from sqlalchemy import Column, Integer, String, Float
+
+
+class ItemModel(db.Model):
+    __tablename__ = "items"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    price = db.Column(db.Float(precision=2), unique=False, nullable=False)
+    store_id = db.Column(
+        db.Integer,
+        db.ForeignKey("stores.id"),
+        unique=False,
+        nullable=False,
+    )
+
+    store = db.relationship("StoreModel", back_populates="items")
+
+    __table_args__ = (
+        db.UniqueConstraint('name', 'store_id'),
+    )
+
+    # id = Column(Integer, primary_key=True)
+    # name = Column(String(80), unique=False, nullable=False)
+    # price = Column(Float(precision=2), unique=False, nullable=False)
+    # store_id = Column(Integer, unique=Flase, nullable=False)
